e326f8d2dc8b899825c05845c4798a893fd05bc0
jwkim <wer07032@naver.com>
2024-10-21 13:57:00 +0900

    git init

---
+++ formwork-shop/src/main/java/com/ndfw/formwork_shop/FormworkShopApplication.java
@@ -0,0 +1,13 @@
+package com.ndfw.formwork_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormworkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormworkShopApplication.class, args);
+	}
+
+}
---
+++ formwork-shop/src/test/java/com/ndfw/formwork_shop/FormworkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndfw.formwork_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormworkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
